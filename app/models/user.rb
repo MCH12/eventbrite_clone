651f795786f410a0a2a27b80c4ff71cfc9350bb4
Rick Thomas <rickthomas1980@gmail.com>
2013-10-05 17:54:58 -0500

    added validations for user and message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password_digest, :username
+
+# validates :email, confirmation: true
+  validates :username, :password_digest, :email, presence: true
+  validates :email, format: { with: /^\S*@\S*\.\w{2,6}$/ }
+  validates :username, length: { in: 3..30 }
+  validates :username, :email, uniqueness: true
 end
