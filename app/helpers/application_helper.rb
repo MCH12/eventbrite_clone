8080253ea331db6e4b1524158f7e32680570cf88
Rick Thomas <rickthomas1980@gmail.com>
2013-10-09 06:05:53 -0500

    added dynamic page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = "EventBrite Clone"
+    if !page_title
+      base_title
+    else
+      base_title + " | #{page_title}"
+    end
+  end
+
 end
